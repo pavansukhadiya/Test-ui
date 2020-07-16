de98927a6323d18172a3a67a818ae6f16c37f536
pavansukhadiya@gmail.com <pavansukhadiya@gmail.com>
2020-07-16 12:01:56 +0100

    Second

---
+++ build/precache-manifest.9664712b5ade4951a53159e83401d74f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61d5eb47c219f778007ec0f99b5aa629",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0873dc8211b1bae333b0",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4234251b1924f0ed17ec",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0873dc8211b1bae333b0",
+    "url": "/static/js/2.e419d85f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e419d85f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4234251b1924f0ed17ec",
+    "url": "/static/js/main.af14e574.chunk.js"
+  },
+  {
+    "revision": "9bbbdfc7ed2f81caeb96",
+    "url": "/static/js/runtime-main.11f1f75c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
